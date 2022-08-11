7055b167f1d24b55170510299aafe8908e00f7fe
zengchao_466695 <466695.nd.com.cn>
2022-08-11 18:15:12 +0800

    feat: 代码缓存

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
   <view class="content">
-    <RecordView @record-end="recordEnd"> </RecordView>
+    <RecordView @record-end="recordEnd"></RecordView>
+    <view class="btn" @click="click">play</view>
   </view>
 </template>
 
@@ -9,12 +10,30 @@ import RecordView from '@/components/record/recordView.vue'
 import { useAudio } from '@/common/utils/useAudio'
 
 const audioCtx = useAudio()
+let path = ''
 
 const recordEnd = (src: string) => {
-  // setTimeout(() => {
-  //   audioCtx.play(src)
-  // }, 1000)
+  path = src
+}
+
+const click = () => {
+  // 语音播放结束需调用 stop，否则下次录音会卡
+  // 录音授权逻辑调整
+  // 录音样式修改
+  audioCtx.play(path)
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.btn {
+  position: absolute;
+  z-index: 100086;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  text-align: center;
+  color: azure;
+  background-color: aqua;
+  line-height: 50px;
+}
+</style>
