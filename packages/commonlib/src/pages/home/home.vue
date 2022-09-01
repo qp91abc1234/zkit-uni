32e87079af625f7747fa2e1725023121ef0aafa0
zengchao_466695 <466695.nd.com.cn>
2022-09-01 16:24:30 +0800

    feat: 代码缓存

---
+++ packages/commonlib/src/pages/home/home.vue
@@ -0,0 +1,51 @@
+<template>
+  <view class="home">
+    <view
+      class="item"
+      v-for="item in demoCfg"
+      :key="item.url"
+      @click="handleClickItem(item.url)"
+      >{{ item.title }}</view
+    >
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const demoCfg = ref([
+  {
+    title: 'record',
+    url: '/pages/record/record'
+  }
+])
+
+const handleClickItem = (url: string) => {
+  uni.navigateTo({
+    url
+  })
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  overflow-y: scroll;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  .item {
+    margin: 30rpx 0;
+    width: 200rpx;
+    height: 100rpx;
+    font-size: 30rpx;
+    border-radius: 13rpx;
+    text-align: center;
+    color: #ffffff;
+    background-color: purple;
+    line-height: 100rpx;
+  }
+}
+</style>
--- packages/demo/src/pages/index/index.vue
+++ packages/demo/src/pages/index/index.vue
@@ -3,12 +3,12 @@
 </template>
 
 <script setup lang="ts">
-import xxx from '@lib/pages/record/index.vue'
+import xxx from '@lib/pages/record/record.vue'
 </script>
 
 <style scoped lang="scss">
-  .main {
-    width:100%;
-    height: 100%;
-  }
+.main {
+  width: 100%;
+  height: 100%;
+}
 </style>
