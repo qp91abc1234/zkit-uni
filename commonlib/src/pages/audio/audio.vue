ac91f2e72cd005f181e6b95da475154a2f3da3cc
zengchao_466695 <466695.nd.com.cn>
2022-10-19 15:50:51 +0800

    feat: 目录逻辑添加

---
+++ commonlib/src/pages/audio/audio.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="audio"></div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<style scoped lang="scss">
+.home {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- commonlib/src/pages/home/home.vue
+++ commonlib/src/pages/home/home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="main">
-    {{ hello }}
+  <div class="home">
+    <button
+      class="item"
+      v-for="item in cfg"
+      :key="item.name"
+      @click="click(item.path)"
+    >
+      {{ item.name }}
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const hello = ref('hello world~')
+const cfg = ref<{ name: string; path: string }[]>([])
+
+const initCfg = () => {
+  const modules = import.meta.glob('../*/*.vue')
+  const modPaths = Object.keys(modules)
+  for (let i = 0; i < modPaths.length; i++) {
+    const path = modPaths[i]
+    if (path === './home.vue') continue
+    cfg.value.push({
+      name: path.slice(path.lastIndexOf('/') + 1, path.lastIndexOf('.')),
+      path: path.replace('../', '/pages/').replace('.vue', '')
+    })
+  }
+}
+
+const click = (path) => {
+  uni.navigateTo({
+    url: path
+  })
+}
+
+initCfg()
 </script>
 
 <style scoped lang="scss">
-.main {
+.home {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: scroll;
   width: 100%;
   height: 100%;
+  .item {
+    margin: 20rpx;
+  }
 }
 </style>
