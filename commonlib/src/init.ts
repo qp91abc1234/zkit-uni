4997afc34e7fc03add183f203ca8b8a539d88151
zcc <350018736@qq.com>
2022-11-02 20:38:49 +0800

    feat: 组件库导出初始化配置

---
+++ commonlib/src/init.ts
@@ -0,0 +1,5 @@
+import '@lib/common/types/global.d'
+import '@lib/common/types/render.d'
+import { setupGlobal } from '@lib/global'
+
+setupGlobal()
--- template/default/src/main.ts
+++ template/default/src/main.ts
@@ -1,6 +1,7 @@
 import { createSSRApp } from 'vue'
 import { setupPinia } from '@/pinia'
 import App from './App.vue'
+import '@lib/init'
 
 export function createApp() {
   const app = createSSRApp(App)
